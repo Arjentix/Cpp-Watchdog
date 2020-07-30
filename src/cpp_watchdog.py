27198dfee0142e1a23acfc47b0280b91d65aa290
Arjentix <arjentix@gmail.com>
2020-07-30 15:52:26 +0300

    Added home and build-command options

--- src/cpp_watchdog.py
+++ src/cpp_watchdog.py
@@ -7,23 +7,40 @@
 
 import sys
 import pyinotify
+import getopt
+
+def get_args(argv):
+    home = '.'
+    build_command = ''
+    files = [home]
+
+    optlist, args = getopt.getopt(argv, 'h:b:', ['home=', 'build-command='])
+    for opt, arg in optlist:
+        if opt in ('-h', '-home'):
+            home = arg
+        elif opt in ('-b', '--build-command'):
+            build_command = arg
+
+    if build_command == '':
+        build_command = f'/usr/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_CC_COMPILER:FILEPATH=/usr/bin/g++ -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -H{home} -B{home}/build -G "Unix Makefiles"'
+    if len(args) != 0:
+        files = args
+
+    return [build_command, files]
 
 def event_handler(event):
     print(f'Something happened: {event}')
 
 if __name__ == "__main__":
-    files = sys.argv[1:]
-    if len(files) == 0:
-        files.append('.') # Setting current dir as default
+    build_command, files = get_args(sys.argv[1:])
 
     # Setting files event handler
     wm = pyinotify.WatchManager()
     notifier = pyinotify.Notifier(wm, event_handler)
     for file in files:
-        wm.add_watch(file, pyinotify.IN_CREATE | pyinotify.IN_DELETE |
+        wm.add_watch(file, pyinotify.IN_CLOSE_WRITE | pyinotify.IN_DELETE |
                            pyinotify.IN_DELETE_SELF | pyinotify.IN_MOVE_SELF|
-                           pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO |
-                           pyinotify.IN_CLOSE_WRITE)
+                           pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO)
 
     try:
         notifier.loop(
